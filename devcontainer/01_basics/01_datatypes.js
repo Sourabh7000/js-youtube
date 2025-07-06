fe8837c04f4aa82fae12aeacd7c3b5f9f74a9b66
Sourabh7000 <sourabhdhote715@gmail.com>
2025-07-06 05:59:13 +0000

    datatypes

---
+++ devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS codr as newer version
+
+//alert(3 + 3);
+
+console.log(3 + 3);
+console.log("Hello World");
+
+// Data Types
+let name = "John"; // String
+let age = 30; // Number
+let isEmployed = true; // Boolean
+let state;
+
+//number = 2 to the power of 53 - 1
+// BigInt
+//string
+//boolean
+// null
+// undefined
+// Symbol
+// Object
+
+console.log(typeof name); // "string"
+console.log(typeof age); // "number"
+console.log(typeof isEmployed); // "boolean"
+console.log(typeof null); // "object" (this is a known bug in JavaScript)
+console.log(typeof state); // "undefined"
