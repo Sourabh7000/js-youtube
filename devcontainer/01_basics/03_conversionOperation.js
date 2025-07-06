2ad6168b72ef717e68202eeeac4ea13c2a97c42a
Sourabh7000 <sourabhdhote715@gmail.com>
2025-07-06 08:38:01 +0000

    conversion of Datatypes

---
+++ devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33add"
+
+console.log(typeof score);
+
+let value = Number(score);
+console.log(typeof value);
+console.log(value);
+
+//"33"=> 33
+//"33add"=> NaN
+
+let isLoggedIn = 1
+console.log(isLoggedIn);
+console.log(Boolean(isLoggedIn)); // true
+
+let someValue = 33
+
+
+let stringValue = String(someValue);
+console.log(stringValue);
